5a1b351d5430a7ff40680d2c90eed7ba5e492230
LubovKos <95122755+LubovKos@users.noreply.github.com>
2023-05-31 22:40:06 +0300

    Search function for nearest available taxi created

--- Taxists_manager.py
+++ Taxists_manager.py
@@ -1,5 +1,6 @@
 import json
 
+import Map
 from Car import Car
 from Order import Order
 from Driver import Driver
@@ -14,19 +15,43 @@ class TaxistManager:
     def download_drivers(self, file: str) -> None:
         with open(file, "r") as read_file:
             data = json.load(read_file)
-        # print('\n')
         for person in data:
             car = person["car"]
             driver_car = Car(car["license_plate"], car["color"], car["brand"], car["capacity"],
                              car["child_seat_availability"], car["category"])
             driver = Driver(driver_car, person["full_name"], person["user_id"], person["location"], person["bank"])
-            self.__free_drivers.append(driver)
-            # driver.print_info()
-            # print('\n')
+            self.__inactive_drivers.append(driver)
 
-    def __search_free_driver(self, order: Order):
+    # переписать
+    def activate_drivers(self):
+        for driver in self.__inactive_drivers:
+            self.__active_drivers.append(driver)
+            # self.__inactive_drivers.remove(driver)
+            driver.print_info()
+        print("_________________________")
+
+    def search_free_driver(self, order: Order, mapp: Map) -> Driver:
         if len(self.__active_drivers) != 0:
-            pass  # надо сделать поиск самого близкого водителя
+            driver_found = False
+            minimal_dist = 1000000
+            # может мы и не найдем нужного водилу (?)
+            for driver in self.__active_drivers:
+                if order.tariff == driver.get_category:
+                    if mapp.calc_distance(driver.get_location, order.get_departure_point()) < minimal_dist:
+                        minimal_dist = mapp.calc_distance(driver.get_location, order.get_departure_point())
+                        suitable_driver = driver
+
+            if driver_found is False:
+                if order.tariff == "economy class":
+                    order.tariff = "comfort class"
+                elif order.tariff == "comfort class":
+                    order.tariff = "business class"
+                else:
+                    for driver in self.__active_drivers:
+                        if map.calc_distance(driver.get_location, order.get_departure_point()) < minimal_dist:
+                            minimal_dist = map.calc_distance(driver.get_location, order.get_departure_point())
+                            suitable_driver = driver
+            return suitable_driver
         else:
             while len(self.__active_drivers) == 0:
                 self.__tick()
