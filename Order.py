96fd3177e285bed0d795313836beb7c0616742f3
Маргаритка <95122842+Oycea@users.noreply.github.com>
2023-06-03 19:51:58 +0300

    Update set_arrival_point and set_rate func in Order.py

--- Order.py
+++ Order.py
@@ -1,69 +1,41 @@
 import uuid
 
-points: list = []
-with open('names.txt') as file:
-    lines = [line.rstrip() for line in file]
-
 
 class Order:
     """docstring"""
-
     def __init__(self):
-        self.__departure_point: str = ""
-        self.__arrival_point: str = ""
-        self.__tariff: str = ""
-        self.__id: uuid = uuid.uuid4()
-        self.__cost = None
+        self.departure_point: str = ""
+        self.arrival_point: str = ""
+        self.tariff: str = ""
+        self.id: uuid = uuid.uuid4()
+        # self.cost = cost (cost will be counting by lower function - calc_cost) (?)
 
     def get_id(self):
-        return self.__id
+        return self.id
 
     def get_departure_point(self):
-        return self.__departure_point
+        return self.departure_point
 
     def get_arrival_point(self):
-        return self.__arrival_point
-
-    @property
-    def get_tariff(self):
-        return self.__tariff
+        return self.arrival_point
 
     def set_depart_point(self, point: str):
-        self.__departure_point = point
+        self.departure_point = point
 
-    def input_arrival_point(self):
+    def print_arrival_point(self, point: str) -> str:
         while True:
             print("Введите конечную точку маршрута:")
-            for i in range(len(points)):
-                print("Пункт №{}: {}".format(i + 1, points[i]))
             arrival_point = input().lower().strip()
-            if arrival_point in points:
-                print("Конечный пункт {} выбран!".format(arrival_point))
+            if 0 <= arrival_point < len(self.__all_arrival_points):
+                print("Конечный пункт выбран!".format(arrival_point))
                 break
             print("Ошибка ввода, повторите!")
             break
-        self.__arrival_point = arrival_point
+        return arrival_point
 
-    def calc_cost(self):
-        pass
-
-    def payment_process(self):
-        pass
-
-    def input_tariff(self, new_tariff='economy class'):
-        self.__tariff = new_tariff
-
-    @get_tariff.setter
-    def get_tariff(self, value):
-        self._get_tariff = value
-
-
-"""
-    def set_tariff(self, tar: str) -> str:
+    def print_tariff(self, tar: str) -> str:
         while True:
             print("Введите тариф:")
-            for i in range(len(self.__all_rate)): #
-                print("Тариф №{}: {}".format(i + 1, self.__all_rate[i]))
             rate = input().lower().strip()
             if 0 <= rate < len(self.__all_rate):
                 print("Тариф №{} выбран!".format(rate))
@@ -71,4 +43,9 @@ class Order:
             print("Ошибка ввода, повторите!")
             break
         return rate
-"""
+
+    def calc_cost(self):
+        pass
+
+    def payment_process(self):
+        pass
