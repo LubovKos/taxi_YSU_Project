abb964d2c06d1085b14505a6706e3718fd54b7cc
Маргаритка <95122842+Oycea@users.noreply.github.com>
2023-06-03 20:01:22 +0300

    Update and rename set_arrival_point and set_tariff func, create input_departure point funcOrder.py

--- Order.py
+++ Order.py
@@ -8,7 +8,6 @@ class Order:
         self.arrival_point: str = ""
         self.tariff: str = ""
         self.id: uuid = uuid.uuid4()
-        # self.cost = cost (cost will be counting by lower function - calc_cost) (?)
 
     def get_id(self):
         return self.id
@@ -19,27 +18,31 @@ class Order:
     def get_arrival_point(self):
         return self.arrival_point
 
-    def set_depart_point(self, point: str):
-        self.departure_point = point
+    def input_depart_point(self):
+        while True:
+            print("Введите начальную точку маршрута:")
+            depart_point = input().lower().strip()
+            if 0 <= depart_point < len(self.__all_depart_points):
+                print("Начальный пункт выбран!".format(depart_point))
+                break
+            print("Ошибка ввода, повторите!")
+            break
+        return depart_point
 
-    def set_arrival_point(self, point: str) -> str:
+    def input_arrival_point(self) -> str:
         while True:
             print("Введите конечную точку маршрута:")
-            for i in range(len(self.__all_arrival_points)): #
-                print("Пункт №{}: {}".format(i + 1, self.__all_arrival_points[i]))
             arrival_point = input().lower().strip()
             if 0 <= arrival_point < len(self.__all_arrival_points):
-                print("Конечный пункт №{} выбран!".format(arrival_point))
+                print("Конечный пункт выбран!".format(arrival_point))
                 break
             print("Ошибка ввода, повторите!")
             break
         return arrival_point
 
-    def set_tariff(self, tar: str) -> str:
+    def input_tariff(self) -> str:
         while True:
             print("Введите тариф:")
-            for i in range(len(self.__all_rate)): #
-                print("Тариф №{}: {}".format(i + 1, self.__all_rate[i]))
             rate = input().lower().strip()
             if 0 <= rate < len(self.__all_rate):
                 print("Тариф №{} выбран!".format(rate))
